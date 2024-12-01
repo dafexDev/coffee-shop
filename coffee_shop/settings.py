6e3485f6840755cc9f2d685b1f75bd487b0d845d
Daniel Fortich <fortichdaniel16@gmail.com>
2024-12-01 10:42:26 -0500

    Media configurations

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -135,3 +135,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# MEDIA
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
